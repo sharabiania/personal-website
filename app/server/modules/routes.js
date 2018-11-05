4ef6f5bae765b2b206427be76fa7948e26860e95
sharabiania <sharabiani.a@gmail.com>
2018-11-05 02:49:34 -0500

    contact page added

--- app/server/modules/routes.js
+++ app/server/modules/routes.js
@@ -6,5 +6,20 @@ module.exports = function (app) {
 	app.use('/project', require('./proj-router')(auth.loggedIn));
 	app.use('/', require('./blog-api')(auth.loggedIn));
 	app.use('/', require('./proj-api')(auth.loggedIn));	
+
+	app.get('/contact', function(req, res){
+		res.render('contact', {sent: false});
+	});
+
+	app.post('/contact', function(req, res){
+		dm = require('../controllers/db-manager')('messages');
+		
+		// TODO: Create a helper function GetIP
+		var uip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+		dm.post({ip: uip, message: req.body.message}, function(dbres){
+			res.render('contact', {sent: true});
+		});
+	});
 	
 }
